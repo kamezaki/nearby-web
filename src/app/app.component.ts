c40d55f7b8f543c23a5c3b66037bfbf0bafea863
Hitoshi Kamezaki <kame@info-cloud.biz>
2017-12-30 21:58:35 +0900

    get authenticated user on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 // import { AngularFireAuth } from 'angularfire2/auth';
 import { Store } from '@ngrx/store';
 import * as firebase from 'firebase/app';
+import * as _ from 'lodash';
 
 import { authUserReducer } from './reducers/';
 import { Logger } from './logging/';
+import { RouterActions } from './actions/index';
 
 @Component({
   selector: 'app-root',
@@ -23,7 +25,11 @@ export class AppComponent implements OnInit {
     this.log.info('ngOnInit');
     this.store.select(authUserReducer.getUser)
       .subscribe(user => {
-        this.log.info(user);
+        if (_.isNull(user) || _.isUndefined(user)) {
+          this.store.dispatch(new RouterActions.Go({ path: ['/login'] }));
+        } else {
+          this.log.info(user);
+        }
       });
 
   }
